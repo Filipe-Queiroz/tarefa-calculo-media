caa2fdd50e97d9b1b1bac56bc65c411b5e815718
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2024-12-02 13:03:44 -0300

    primeiro commit

---
+++ src/MediaDeNotas.java
@@ -0,0 +1,19 @@
+public class MediaDeNotas {
+    public static void main(String[] args) {
+
+        double nota1 = 9;
+
+        double nota2 = 9;
+
+        double nota3 = 7;
+
+        double nota4 = 5;
+
+        double somaDasNotas = nota1 + nota2 + nota3 + nota4;
+
+        double calculoMediaNotas = somaDasNotas / 4;
+
+        System.out.println("A soma das notas: " + somaDasNotas);
+        System.out.println("A media das notas: " + calculoMediaNotas);
+    }
+}
